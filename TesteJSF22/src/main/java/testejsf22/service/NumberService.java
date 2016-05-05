5f8445edc3544dbd0974cb2414618129bfb1da07
patrizio <patrizio@patrizio-desktop>
2016-05-04 22:15:11 -0300

    Teste de ExceptionHandler

---
+++ TesteJSF22/src/main/java/testejsf22/service/NumberService.java
@@ -0,0 +1,13 @@
+package testejsf22.service;
+
+import java.math.BigDecimal;
+
+/**
+ * @author patrizio
+ * @since 03/05/2016
+ */
+public interface NumberService {
+
+	BigDecimal calculateDivision(BigDecimal dividend, BigDecimal divisor);
+	
+}
---
+++ TesteJSF22/src/main/java/testejsf22/utils/CustomExceptionHandlerFactory.java
@@ -0,0 +1,23 @@
+package testejsf22.utils;
+
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.ExceptionHandlerFactory;
+
+/**
+ * @author patrizio
+ * @since 02/05/2016
+ */
+public class CustomExceptionHandlerFactory extends ExceptionHandlerFactory {
+
+	private ExceptionHandlerFactory parent;
+
+	public CustomExceptionHandlerFactory(ExceptionHandlerFactory parent) {
+		this.parent = parent;
+	}
+
+	@Override
+	public ExceptionHandler getExceptionHandler() {
+		return new CustomExceptionHandler(parent.getExceptionHandler());
+	}
+
+}
