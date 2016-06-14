f99c8fc6b0c5716ef0e70becc61f1fec32da52f2
patriziocannoni <patrizio.cannoni@gmail.com>
2016-06-14 12:54:43 -0300

    Nova classe CarroInfo para teste de relacionamento OneToOne

--- TesteJSF22/src/main/java/testejsf22/model/Carro.java
+++ TesteJSF22/src/main/java/testejsf22/model/Carro.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 /**
  * @author patrizio
@@ -25,6 +26,9 @@ public class Carro {
 
 	@Column
 	private Integer ano;
+	
+	@OneToOne(optional = true)
+	private CarroInfo carroInfo;
 
 	public Long getId() {
 		return id;
---
+++ TesteJSF22/src/main/java/testejsf22/model/CarroInfo.java
@@ -0,0 +1,26 @@
+package testejsf22.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class CarroInfo {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String cor;
+    
+    @Column
+    private Integer kilometragem;
+    
+    @OneToOne(optional = false)
+    private Carro carro;
+    
+}
