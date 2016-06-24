4d1af3162b73b6fd36bec45532e072e7c86f57e9
patrizio <patrizio@patrizio-desktop>
2016-06-23 23:00:59 -0300

    Nova página de login e ajustes no menu principal.

---
+++ TesteJSF22/src/main/java/testejsf22/view/UserSessionMB.java
@@ -0,0 +1,17 @@
+package testejsf22.view;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class UserSessionMB implements Serializable {
+
+	private static final long serialVersionUID = 3721957783771000407L;
+
+	public String logout() {
+		return "home";
+	}
+}
