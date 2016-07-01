6c0c1230fd3a0a33b16e8fa6e74d6d7e114be437
patriziocannoni <patrizio.cannoni@gmail.com>
2016-07-01 16:02:05 -0300

    Melhorado o RedirectNavigationHandler quando a passagem de parâmetros via URL.

--- TesteJSF22/src/main/java/testejsf22/controller/RedirectNavigationHandler.java
+++ TesteJSF22/src/main/java/testejsf22/controller/RedirectNavigationHandler.java
@@ -13,40 +13,39 @@ import javax.faces.context.FacesContext;
  * @since 19/12/2014
  */
 public class RedirectNavigationHandler extends ConfigurableNavigationHandler {
+    
+    private static final String NAVIGATION_REDIRECT_SUFFIX = "faces-redirect=true";
+    
+    private static final String NAVIGATION_REDIRECT_SUFFIX_URL = "?" + NAVIGATION_REDIRECT_SUFFIX;
 
-	private static final String NAVIGATION_REDIRECT_SUFFIX = "?faces-redirect=true";
+    private NavigationHandler parent;
 
-	private NavigationHandler parent;
+    public RedirectNavigationHandler(NavigationHandler parent) {
+        this.parent = parent;
+    }
 
-	public RedirectNavigationHandler(NavigationHandler parent) {
-		this.parent = parent;
-	}
+    @Override
+    public void handleNavigation(FacesContext context, String from, String outcome) {
+        if (outcome != null && outcome.indexOf(NAVIGATION_REDIRECT_SUFFIX) == -1) {
+            outcome += NAVIGATION_REDIRECT_SUFFIX_URL;
+        }
+        parent.handleNavigation(context, from, outcome);
+    }
 
-	@Override
-	public void handleNavigation(FacesContext context, String from, String outcome) {
-		if (outcome != null && !outcome.endsWith(NAVIGATION_REDIRECT_SUFFIX)) {
-			outcome += NAVIGATION_REDIRECT_SUFFIX;
-		}
+    @Override
+    public NavigationCase getNavigationCase(FacesContext context, String fromAction, String outcome) {
+        if (parent instanceof ConfigurableNavigationHandler) {
+            return ((ConfigurableNavigationHandler) parent).getNavigationCase(context, fromAction, outcome);
+        }
+        return null;
+    }
 
-		parent.handleNavigation(context, from, outcome);
-	}
-
-	@Override
-	public NavigationCase getNavigationCase(FacesContext context, String fromAction, String outcome) {
-		if (parent instanceof ConfigurableNavigationHandler) {
-			return ((ConfigurableNavigationHandler) parent).getNavigationCase(context, fromAction, outcome);
-		} else {
-			return null;
-		}
-	}
-
-	@Override
-	public Map<String, Set<NavigationCase>> getNavigationCases() {
-		if (parent instanceof ConfigurableNavigationHandler) {
-			return ((ConfigurableNavigationHandler) parent).getNavigationCases();
-		} else {
-			return null;
-		}
-	}
+    @Override
+    public Map<String, Set<NavigationCase>> getNavigationCases() {
+        if (parent instanceof ConfigurableNavigationHandler) {
+            return ((ConfigurableNavigationHandler) parent).getNavigationCases();
+        }
+        return null;
+    }
 
 }
